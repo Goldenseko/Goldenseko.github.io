813547bceb01b0a7b7398603266b33b2d4deb757
Goldenseko <sekomarcoshigeki@gmail.com>
2018-03-17 09:58:07 +0900

    Uploading lots of files

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,18 @@
-var x = 10
 
-alert(console.log(x));
+function draw() {
+  var myCanvas = document.getElementById('canvas');
+  var ctx = myCanvas.getContext('2d');
+
+
+  ctx.fillStyle = "rgb(200,0,0)";
+  ctx.fillRect (10, 10, 55, 50);
+
+  document.getElementById("canvassample");
+
+  for (var i=0; i < 8; i++){
+    for(var x=0; x<8; x++){
+      drawGrid(context, x, y);
+    }　
+      drawGrid(context, x, y);
+  }
+}
